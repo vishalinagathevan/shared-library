9f1373de4459d832478b9a32828cb5f80226a0e9
vishalinagathevan <126974938+vishalinagathevan@users.noreply.github.com>
2023-07-25 12:45:30 +0530

    Create calculator.groovy

---
+++ vars/calculator.groovy
@@ -0,0 +1,7 @@
+def add(number1, number2){
+  echo "sum of $(number1) and $(number2) is $(number1+number2)"
+}
+
+def multiply(number1, number2){
+  echo "multiplication of $(number1) and $(number2) is $(number1*number2)"
+}  
